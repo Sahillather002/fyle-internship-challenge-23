1b40932394e462c6758d51eb5151fd23dc62b850
SahilGrip <sahillather002@gmail.com>
2024-05-11 08:38:35 +0530

    step the intro page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,17 @@ import { ApiService } from './services/api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  constructor(
-    private apiService: ApiService
-  ) {}
+export class AppComponent implements OnInit {
+  username: string = '';
+
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+    this.apiService.getUser('sahil').subscribe(console.log);
+  }
+  onSubmit() {
+    alert('Hello to project');
   }
 }
